bd92446c44d3bc4e88d805925c3d52b027024552
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2021-09-30 11:12:11 +0200

    without headers

--- src/components/sections/eventcontent.js
+++ src/components/sections/eventcontent.js
@@ -221,7 +221,7 @@ export default function EventContent({ data }) {
             onSubmit={(data, {resetForm}) => {
               fetch("/", {
                 method: "POST",
-                headers: { "Content-Type": 'multipart/form-data' },
+              //  headers: { "Content-Type": 'multipart/form-data' },
                 body: encode({
                   "form-name": "art-doc",
                   ...data,
